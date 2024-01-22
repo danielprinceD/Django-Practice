c1daa30466100dbd0262b92ebfb028235af9c381
daniel <d.danielprince2003@gmail.com>
2024-01-22 19:31:35 +0530

    feat:JSON Response using api

--- RESTFrameWork/backend/backend/api/views.py
+++ RESTFrameWork/backend/backend/api/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 # Create your views here.
-
+import json 
 def hello(request):
-    return JsonResponse(
-        {
-            "messgae" : "Hello This is Rest API"
-        }
-    )
+    query = {}
+    
+    query['type'] = request.content_type 
+    query['server_name'] = "my_server" 
+    query['GET'] = dict(request.GET)
+    query['request'] = dict(request.body)
+    query['headers'] = dict(request.headers)
+    return JsonResponse(query)
--- RESTFrameWork/client/tut.py
+++ RESTFrameWork/client/tut.py
@@ -1,7 +1,6 @@
 import requests
  
 end  = 'http://127.0.0.1:8000/api'
-obj = requests.get(end)
-print(obj.text)
-print(obj.status_code)
-print(obj.json()["messgae"])
+obj = requests.get(end,params={'this_is_param':'get_method'},json={'query':'Hello'})
+print(obj.url)
+print(obj.json())
