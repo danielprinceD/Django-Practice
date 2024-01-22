f0b07c526c849b3da43507237a5805c4bb51b84e
daniel <d.danielprince2003@gmail.com>
2024-01-22 21:01:37 +0530

    feat:rest-framework decorators and response

--- RESTFrameWork/backend/backend/api/views.py
+++ RESTFrameWork/backend/backend/api/views.py
@@ -3,7 +3,10 @@ from django.http import JsonResponse
 from django.forms.models import model_to_dict
 # Create your views here.
 import json 
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from Product.models import Product
+@api_view(["POST"])
 def hello(request):
     query = {}
     product = Product.objects.all().order_by("?").first()
@@ -11,4 +14,4 @@ def hello(request):
     query['prod_price'] = product.price
     
     newDate = model_to_dict(product)
-    return JsonResponse(newDate)
+    return Response(newDate)
--- RESTFrameWork/backend/backend/backend/settings.py
+++ RESTFrameWork/backend/backend/backend/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
-    'Product'
+    'Product',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
