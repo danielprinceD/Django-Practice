019ebbfed62b1dc00534c6d65d2d1eed7f72ade5
daniel <d.danielprince2003@gmail.com>
2024-01-24 21:15:15 +0530

    feat:Login Register and Home Page is completed

--- List_Application/List_Application/settings.py
+++ List_Application/List_Application/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 import os
-TEMP_DIR = os.path.dirname(os.path.dirname((os.path.abspath(__file__))))+"\\Template"
+TEMP_DIR = os.path.join(os.path.dirname(os.path.dirname((os.path.abspath(__file__)))), 'Template')
 print(TEMP_DIR)
 
 # Quick-start development settings - unsuitable for production
--- List_Application/List_Application/views.py
+++ List_Application/List_Application/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
 def home(request):
-    return HttpResponse(request,'home.html')
+    return render(request,'home.html')
