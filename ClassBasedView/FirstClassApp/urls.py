ff190666f0c7c63419b475ab89de351faa08d79a
daniel <d.danielprince2003@gmail.com>
2024-01-25 10:39:24 +0530

    feat:Detail View

--- ClassBasedView/FirstClassApp/urls.py
+++ ClassBasedView/FirstClassApp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import StudentList
+from .views import StudentList,StudentDetail
 urlpatterns = [
-    path('ListClass',StudentList.as_view())
+    path('ListClass',StudentList.as_view()),
+    path('DetailClass/<int:pk>',StudentDetail.as_view())
 ]
--- ClassBasedView/FirstClassApp/views.py
+++ ClassBasedView/FirstClassApp/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render,HttpResponse
 from .models import Student
-from django.views.generic import ListView
-
+from django.views.generic import(
+    ListView,
+    DetailView
+)
 class StudentList(ListView):
+    queryset = Student.objects.all()
+    
+class StudentDetail(DetailView):
     queryset = Student.objects.all()
         
