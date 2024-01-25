5a2d71007b5b44d6e1026d5920d476b7aa345b78
daniel <d.danielprince2003@gmail.com>
2024-01-25 11:05:30 +0530

    feat:Detail View is completed

--- ClassBasedView/FirstClassApp/urls.py
+++ ClassBasedView/FirstClassApp/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import StudentList,StudentDetail
 urlpatterns = [
     path('ListClass',StudentList.as_view()),
-    path('DetailClass/<int:pk>',StudentDetail.as_view())
+    path('DetailClass/<int:id>',StudentDetail.as_view())
 ]
--- ClassBasedView/FirstClassApp/views.py
+++ ClassBasedView/FirstClassApp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,get_object_or_404
 from .models import Student
 from django.views.generic import(
     ListView,
@@ -9,4 +9,8 @@ class StudentList(ListView):
     
 class StudentDetail(DetailView):
     queryset = Student.objects.all()
+    def get_object(self):
+        id_ = self.kwargs.get("id")
+        return get_object_or_404(Student,id = id_)
+    
         
