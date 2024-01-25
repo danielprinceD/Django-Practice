43b098600af35d85a5f75a0f0f350a7152560ad9
daniel <d.danielprince2003@gmail.com>
2024-01-25 12:57:57 +0530

    feat:Delete View completed

--- ClassBasedView/FirstClassApp/urls.py
+++ ClassBasedView/FirstClassApp/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import StudentList,StudentDetail,StudentCreate,StudentUpdate
+from .views import (
+    StudentList,StudentDetail,StudentCreate,StudentUpdate,StudentDelete
+)
 urlpatterns = [
     path('ListClass',StudentList.as_view(),name="ListView"),
     path('DetailClass/<int:id>',StudentDetail.as_view() , name = "DetailView"),
     path('CreateView',StudentCreate.as_view()),
-    path('<int:id>/UpdateView',StudentUpdate.as_view())
+    path('<int:id>/UpdateView',StudentUpdate.as_view()),
+    path('DeleteView/<int:id>',StudentDelete.as_view())
 ]
--- ClassBasedView/FirstClassApp/views.py
+++ ClassBasedView/FirstClassApp/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render,HttpResponse,get_object_or_404
 from .models import Student
+from django.urls import reverse
 from django.views.generic import(
     ListView,
     DetailView,
     CreateView,
-    UpdateView
+    UpdateView,
+    DeleteView
 )
 from .forms import StudentForm
 class StudentList(ListView):
@@ -29,3 +31,13 @@ class StudentUpdate(UpdateView):
     def get_object(self):
         id_ = self.kwargs.get("id")
         return get_object_or_404(Student,id=id_)
+    
+class StudentDelete(DeleteView):
+    template_name = 'FirstClassApp/student_delete.html'
+    
+    def get_object(self):
+        id_ = self.kwargs.get("id")
+        return get_object_or_404(Student,id = id_)
+    
+    def get_success_url(self):
+        return reverse("ListView")
