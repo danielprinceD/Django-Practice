9e8aa24e0cfaf9928332d815e665992d06c0f28c
danielprinced <d.danielprince2003@gmail.com>
2025-05-11 17:32:50 +0530

    change root project directory

--- 2025/learn/settings.py
+++ 2025/learn/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-wb(o)d*l4x*ahp2$o_lk#zmzhj9f&vi_h-3ddc4pe_0gb@!c40"
+SECRET_KEY = "django-insecure-2vb1o8m#&+s*xff9!of@ydaak+cu9%448-0$1zlo9aj9&i&@p@"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
